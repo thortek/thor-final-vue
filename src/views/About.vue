6c80fe9e7bd521bee7707063f9472d1f12cb0e9e
Thor Anderson <ta.anderson@gmail.com>
2018-10-15 15:34:23 -0600

    Added Swapi data files and made simple list render

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,27 @@
 <template>
-  <div class="about">
+  <div >
     <h1>This is an about page</h1>
+    <ul id="example-1">
+      <li v-for="film in allFilms" :key="film.episode_id">
+        {{ film.title }}
+      </li>
+</ul>
   </div>
 </template>
+
+<script>
+import { films } from '../assets/films'
+
+export default {
+  data: function() {
+    return {
+      allFilms: films
+    }
+  }
+}
+</script>
+
+<style>
+</style>
+
+
