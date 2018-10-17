5d846b4058ebfe11e9c211fca125cd3788bf10d1
Thor Anderson <ta.anderson@gmail.com>
2018-10-17 14:25:51 -0600

    grid view

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,25 @@
 <template>
-  <div >
+<v-container grid-list-md text-xs-center>
+  <v-layout row wrap>
+    <v-flex xs3 v-for="ship in starships" :key="ship.model">
+      <v-card >
+        <v-card-title primary-title>
+          <div>
+            <h3 class="headline mb-0">{{ship.name}}</h3>
+            <div>{{ ship.model}}</div>
+            <span>{{ ship.manufacturer }}</span>
+          </div>
+        </v-card-title>
+
+        <v-card-actions>
+          <v-btn :click="followLink(ship.name)" flat color="blue">Search</v-btn>
+        </v-card-actions>
+      </v-card>
+
+    </v-flex>
+  </v-layout>
+</v-container>
+  <!-- <div >
     <h1>This is an about page</h1>
     <ul id="example-1">
       <li v-for="film in allFilms" :key="film.episode_id">
@@ -7,7 +27,7 @@
       </li>
 </ul>
 <div>{{ starships[0]}}</div>
-  </div>
+  </div> -->
 </template>
 
 <script>
@@ -19,6 +39,11 @@ export default {
     return {
       allFilms: films,
       starships
+    }
+  },
+  methods: {
+    followLink: function (name) {
+      router.push({path: 'https://www.google.com/search?q=cake&tbm=isch'})
     }
   }
 }
