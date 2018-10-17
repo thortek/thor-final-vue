c03675afe591b47862d69a8f6970c0e1b2fb68c3
Thor Anderson <ta.anderson@gmail.com>
2018-10-17 13:40:20 -0600

    Added starships

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,19 +3,22 @@
     <h1>This is an about page</h1>
     <ul id="example-1">
       <li v-for="film in allFilms" :key="film.episode_id">
-        {{ film.title }}
+        {{ film.opening_crawl }}
       </li>
 </ul>
+<div>{{ starships[0]}}</div>
   </div>
 </template>
 
 <script>
 import { films } from '../assets/films'
+import { starships } from '../assets/starships'
 
 export default {
   data: function() {
     return {
-      allFilms: films
+      allFilms: films,
+      starships
     }
   }
 }
