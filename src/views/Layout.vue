5107c6c87b28e099e1e5b6db4ddc8066b26aeb9d
Thor Anderson <ta.anderson@gmail.com>
2018-10-24 14:43:57 -0600

    Latest layout changes

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,15 +1,28 @@
 <template>
-    <h1>This is where I'll practice some layout stuff</h1>
+    <v-app>
+  <v-navigation-drawer app clipped v-model="drawer">Nav</v-navigation-drawer>
+  <v-toolbar app clipped-left>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+  </v-toolbar>
+  <v-content>
+<v-container fluid>
+<div>My content goes here</div>
+    </v-container>
+  </v-content>
+  <v-footer app>Footer</v-footer>
+</v-app>
 </template>
 
 <script>
-
-export default {
-    
-}
+ export default {
+    data () {
+      return {
+        drawer: null,
+      }
+    }
+  }
 </script>
 
 <style scoped>
-
 </style>
 
