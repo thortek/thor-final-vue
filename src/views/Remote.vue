d8049849d3e9365a0a29fcac7b7293dc5bfbba2f
Thor Anderson <ta.anderson@gmail.com>
2018-11-14 13:45:42 -0700

    Expansion panel layout

--- src/views/Remote.vue
+++ src/views/Remote.vue
@@ -1,8 +1,18 @@
 <template>
-    <v-container>
+    <v-container grid-list-md text-xs-center>
     <h1>Remote DB access example</h1>
     <v-btn @click="getCourses">Get Courses</v-btn>
-    <div>{{ courses }}</div>
+<v-expansion-panel focusable>
+    <v-expansion-panel-content
+      v-for="course in courses"
+      :key="course.key"
+    >
+      <div slot="header">{{course.prefix}} {{course.number}} {{course.title}}</div>
+      <v-card>
+        <v-card-text class="grey lighten-3">{{ course.description }}</v-card-text>
+      </v-card>
+    </v-expansion-panel-content>
+  </v-expansion-panel>
     </v-container>
 </template>
 
@@ -16,7 +26,8 @@ export default {
     }
   },
   methods: {
-    getCourses() {axios.get('https://sweltering-fire-2420.firebaseio.com/courses.json')
+    getCourses() {
+      return axios.get('https://sweltering-fire-2420.firebaseio.com/courses.json')
         .then(response => { // wow, diff between es5 function and arrow
           console.log(response)
           const allCoursesObj = response.data
